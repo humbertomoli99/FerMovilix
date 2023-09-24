6581f5eb67ec4645b7d1b03780a9cde3ff3d49a8
humberto <humberto_molinalopez@hotmail.com>
2023-09-23 22:48:48 -0600

    Agregar archivos de proyecto.

---
+++ FerMovilix/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FerMovilix
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FerMovilix/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FerMovilix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
